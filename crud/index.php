d46ecc3553725d8011d5af26a634d72ab68370e5
sanjib00 <sanjib_1@hotmail.com>
2014-08-17 11:45:45 +0600

    intial commit

---
+++ crud/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form>
+            <fieldset>
+                <legend>FTFL BATCH</legend>
+                <label>
+                    <span>
+                        Batch Name
+                    </span>
+                    <input type="text" name="batch_name"/>
+                </label>
+                <label>
+                    <input type="submit" value="Submit">
+                </label>
+            </fieldset>
+        </form>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
