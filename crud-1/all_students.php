4533612245a2fa9edabac3b2f9dcbf445dd2bac1
sanjib00 <sanjib_1@hotmail.com>
2014-08-18 23:32:52 +0600

    CRUD exercise

---
+++ crud-1/all_students.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="http://getbootstrap.com/favicon.ico">
+
+    <title>SHAL Solutions Ltd.</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="Dashboard%20Template%20for%20Bootstrap_files/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="Dashboard%20Template%20for%20Bootstrap_files/dashboard.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="Dashboard%20Template%20for%20Bootstrap_files/ie-emulation-modes-warning.js"></script>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="Dashboard%20Template%20for%20Bootstrap_files/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  <style type="text/css" id="holderjs-style"></style></head>
+
+  <body>
+   <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">FTFL Management System</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="#">Settings</a></li>
+            <li><a href="#">Profile</a></li>
+            <li><a href="#">Help</a></li>
+          </ul>
+          <form class="navbar-form navbar-right">
+            <input class="form-control" placeholder="Search..." type="text">
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li class="active"><a href="#">MENU</a></li>
+            <li><a href="add_new.html">Add Student</a></li>
+            <li><a href="add_courses.html">Add New Courses</a></li>
+            <li><a href="all_students.html">View All Students</a></li>
+			<li><a href="all_courses.html">View All Courses</a></li>
+			<li><a href="#">Forum</a></li>
+		    <li><a href="#">Contact Us</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            
+            <li><a href="">Profile</a></li>
+			<li><a href="">Messages</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+          
+            <li><a href="">Notifications</a></li>
+          </ul>
+        </div>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">FTFL Management System</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="index.html">Dashboard</a></li>
+            <li><a href="#">Settings</a></li>
+            <li><a href="#">Profile</a></li>
+            <li><a href="#">Help</a></li>
+          </ul>
+          <form class="navbar-form navbar-right">
+            <input class="form-control" placeholder="Search..." type="text">
+          </form>
+        </div>
+      </div>
+    </div>
+
+    
+        <div class="col-sm-9 col-sm-offset-3 col-md-12 col-md-offset-2 main">
+         			
+		 <div class="panel-body">
+            <?php
+
+            mysql_connect('localhost','root','');
+
+            mysql_select_db('ftfl');
+
+            $qury = mysql_query("SELECT *
+                    from students
+                    Order by students.id");
+
+            echo "<table class='table table-condensed'>";
+            echo "<thead>";
+            echo "<tr>";
+            echo "<th>ID</th>";
+            echo "<th>STUDENT NAME</th>";
+            echo "<th>ACTION</th>";
+            echo "</tr>";
+            echo "</thead>";
+            while ($data = mysql_fetch_object($qury)) {
+                echo "<tr>";
+                echo "<td>".$data->id."</td>";
+                echo "<td>".$data->name."</td>";
+                //echo "<td>".$data->title."</td>";
+                echo "<div class='btn-group btn-group-justified'>";
+                echo "<td><a href='view_Student.php?student_Id=".$data->id."' class='btn btn-primary' role='button'>VIEW</a></td>";
+                echo "<td><a href='edit_One_Student.php?student_Id=".$data->id."' class='btn btn-primary' role='button'>EDIT</a></td>";
+                echo "<td><a href='delete_One_Student.php?student_Id=".$data->id."' class='btn btn-primary' role='button'>DELETE</a></td>";
+                echo "<td><a href='assign_Student_Course.php?student_Id=".$data->id."' class='btn btn-primary' role='button'>ASSIGN COURSE</a></td>";
+                echo "</div>";
+                //echo "<td><a href='view.php?id=".$data->id."'>View</a></td>";
+                //echo "<td><a href='edit.php?id=".$data->id."'>Edit</a></td>";
+                //echo "<td><a href='delete.php?id=".$data->id."'>Delete</a></td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+
+
+
+            ?>
+
+
+
+        </div>
+			
+      </div>
+
+          
+        </div>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="Dashboard%20Template%20for%20Bootstrap_files/jquery.js"></script>
+    <script src="Dashboard%20Template%20for%20Bootstrap_files/bootstrap.js"></script>
+    <script src="Dashboard%20Template%20for%20Bootstrap_files/docs.js"></script>
+  
+
+<div data-original-title="Copy to clipboard" title="" style="position: absolute; left: 0px; top: -9999px; width: 15px; height: 15px; z-index: 999999999;" class="global-zeroclipboard-container" id="global-zeroclipboard-html-bridge">      <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" id="global-zeroclipboard-flash-bridge" height="100%" width="100%">         <param name="movie" value="/assets/flash/ZeroClipboard.swf?noCache=1407987260558">         <param name="allowScriptAccess" value="sameDomain">         <param name="scale" value="exactfit">         <param name="loop" value="false">         <param name="menu" value="false">         <param name="quality" value="best">         <param name="bgcolor" value="#ffffff">         <param name="wmode" value="transparent">         <param name="flashvars" value="trustedOrigins=getbootstrap.com%2C%2F%2Fgetbootstrap.com%2Chttp%3A%2F%2Fgetbootstrap.com">         <embed src="Dashboard%20Template%20for%20Bootstrap_files/ZeroClipboard.swf" loop="false" menu="false" quality="best" bgcolor="#ffffff" name="global-zeroclipboard-flash-bridge" allowscriptaccess="sameDomain" allowfullscreen="false" type="application/x-shockwave-flash" wmode="transparent" pluginspage="http://www.macromedia.com/go/getflashplayer" flashvars="trustedOrigins=getbootstrap.com%2C%2F%2Fgetbootstrap.com%2Chttp%3A%2F%2Fgetbootstrap.com" scale="exactfit" height="100%" width="100%">                </object></div></body></html>
---
+++ crud-1/index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="http://getbootstrap.com/favicon.ico">
+
+    <title>SHAL Solutions Ltd.</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="Dashboard%20Template%20for%20Bootstrap_files/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="Dashboard%20Template%20for%20Bootstrap_files/dashboard.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="Dashboard%20Template%20for%20Bootstrap_files/ie-emulation-modes-warning.js"></script>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="Dashboard%20Template%20for%20Bootstrap_files/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  <style type="text/css" id="holderjs-style"></style></head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">FTFL Management System</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="#">Settings</a></li>
+            <li><a href="#">Profile</a></li>
+            <li><a href="#">Help</a></li>
+          </ul>
+          <form class="navbar-form navbar-right">
+            <input class="form-control" placeholder="Search..." type="text">
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-2 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li class="active"><a href="#">MENU</a></li>
+            <li><a href="add_new.php">Add Student</a></li>
+            <li><a href="add_courses.php">Add New Courses</a></li>
+            <li><a href="all_students.php">View All Students</a></li>
+			<li><a href="all_courses.php">View All Courses</a></li>
+			<li><a href="#">Forum</a></li>
+		    <li><a href="#">Contact Us</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            
+            <li><a href="">Profile</a></li>
+			<li><a href="">Messages</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+          
+            <li><a href="">Notifications</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-6 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <h1 class="page-header">Dashboard</h1>
+
+            <div class="col-xs-6 col-sm-5 col-md-4 placeholder">
+              <img src="image/graph.gif"class="img-responsive" alt="200x200">
+              <h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Three Student Registered</h4>
+              <span class="text-muted"></span>
+            </div>
+            <div class="col-xs-6 col-sm-5 col-md-4 placeholder">
+              <img src="image/pie-graphs.jpg" class="img-responsive" alt="200x200">
+              <h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Five Courses Available</h4>
+              <span class="text-muted"></span>
+            </div>
+			
+		<table class="table table-striped">
+				
+				<thead>
+					<tr>
+						<th>Student Section</th>
+						<th>Courses</th>
+					</tr>
+				</thead>
+				
+			<tbody>
+			
+				<tr>
+					<td><a href = "all_students.php"> View All Students</a></td>
+					<td><a href = "all_courses.php">View All Courses</a></td>
+				</tr>
+			
+				<tr>
+					<td><a href = "add_new.php">Add New Students</a></td>
+					<td><a href = "add_courses.php">Add New Courses</a></td>
+					
+				</tr>
+				<tr>
+					<td><a href = "assign_student.php">Assign Student with courses</a></td>
+					<td><a href = "assign_courses.php">Assign Courses to Student</a></td>
+					
+				</tr>
+				
+			</tbody>
+		</table>
+			
+      </div>
+
+          
+        </div>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="Dashboard%20Template%20for%20Bootstrap_files/jquery.js"></script>
+    <script src="Dashboard%20Template%20for%20Bootstrap_files/bootstrap.js"></script>
+    <script src="Dashboard%20Template%20for%20Bootstrap_files/docs.js"></script>
+  
+
+<div data-original-title="Copy to clipboard" title="" style="position: absolute; left: 0px; top: -9999px; width: 15px; height: 15px; z-index: 999999999;" class="global-zeroclipboard-container" id="global-zeroclipboard-html-bridge">      <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" id="global-zeroclipboard-flash-bridge" height="100%" width="100%">         <param name="movie" value="/assets/flash/ZeroClipboard.swf?noCache=1407987260558">         <param name="allowScriptAccess" value="sameDomain">         <param name="scale" value="exactfit">         <param name="loop" value="false">         <param name="menu" value="false">         <param name="quality" value="best">         <param name="bgcolor" value="#ffffff">         <param name="wmode" value="transparent">         <param name="flashvars" value="trustedOrigins=getbootstrap.com%2C%2F%2Fgetbootstrap.com%2Chttp%3A%2F%2Fgetbootstrap.com">         <embed src="Dashboard%20Template%20for%20Bootstrap_files/ZeroClipboard.swf" loop="false" menu="false" quality="best" bgcolor="#ffffff" name="global-zeroclipboard-flash-bridge" allowscriptaccess="sameDomain" allowfullscreen="false" type="application/x-shockwave-flash" wmode="transparent" pluginspage="http://www.macromedia.com/go/getflashplayer" flashvars="trustedOrigins=getbootstrap.com%2C%2F%2Fgetbootstrap.com%2Chttp%3A%2F%2Fgetbootstrap.com" scale="exactfit" height="100%" width="100%">                </object></div></body></html>
